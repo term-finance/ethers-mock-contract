406ad31ac7aa7e303daabb8b752b8d9aa97d90e9
Robert Chu <robert@termfinance.io>
2024-08-01 10:27:56 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import {
+export {
   MockReadCallExpectation,
   MockRevertExpectation,
   MockCallExpectation,
   MockContract,
   deployMock,
 } from "./mock-controller.ts"
-
-export MockReadCallExpectation;
-export MockRevertExpectation;
-export MockCallExpectation;
-export MockContract;
-export deployMock;
