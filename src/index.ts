0c59dbfa654b57f305a71ddd8ca4b374f1ddcfce
robotoer <1199079+robotoer@users.noreply.github.com>
2025-05-02 04:17:30 +0000

    [stylebot] Fixes for code style

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,7 @@ export {
   deployMock,
 } from "./mock-contract";
 
-export { type MockContract as MockWaffleContract, deployMockContract } from "./compat/waffle.js";
+export {
+  type MockContract as MockWaffleContract,
+  deployMockContract,
+} from "./compat/waffle.js";
